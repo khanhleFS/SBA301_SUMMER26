237dbc9a451acfce84441accb259a5a6a2d479a0
TranNhatGiaHuy <trannhatgiahuyjob@gmail.com>
2026-05-29 21:41:50 +0700

    (ADMIN) CRUD for Category

--- SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/repository/CategoryRepository.java
+++ SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/repository/CategoryRepository.java
@@ -3,7 +3,11 @@ package com.fpt.sba301_su26_groupproject.repository;
 import com.fpt.sba301_su26_groupproject.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, UUID id);
+    Optional<Category> findBySlug(String slug);
 }
