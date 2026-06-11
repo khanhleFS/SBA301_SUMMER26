2eb6ff396295741572ac54a1e1bc6e51048991e1
minchit <trietpm.ce190589@gmail.com>
2026-06-11 22:27:13 +0700

    feat(payment): add momo config and payment DTOs

---
+++ SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/dto/payment/PaymentMomoCreateRequestDTO.java
@@ -0,0 +1,20 @@
+package com.fpt.sba301_su26_groupproject.dto.payment;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentMomoCreateRequestDTO(
+        @NotBlank
+        @Schema(example = "ORDER001")
+        String orderId,
+
+        @NotNull
+        @Min(100)
+        @Schema(example = "100000")
+        Integer amount,
+
+        @Schema(example = "Test payment")
+        String orderInfo
+) {}
