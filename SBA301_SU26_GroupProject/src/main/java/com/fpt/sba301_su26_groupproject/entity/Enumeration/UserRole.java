4037c61465611feb2619c5653e2b64ba5d91add5
TranNhatGiaHuy <trannhatgiahuyjob@gmail.com>
2026-07-16 11:08:51 +0700

    [FIX] delete Author role

--- SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/entity/Enumeration/UserRole.java
+++ SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/entity/Enumeration/UserRole.java
@@ -2,6 +2,5 @@ package com.fpt.sba301_su26_groupproject.entity.Enumeration;
 
 public enum UserRole {
     ADMIN,
-    AUTHOR,
     USER
 }
--- SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/entity/User.java
+++ SBA301_SU26_GroupProject/src/main/java/com/fpt/sba301_su26_groupproject/entity/User.java
@@ -45,4 +45,10 @@ public class User extends BaseEntity {
     @Column(name = "coin_balance", nullable = false)
     @Builder.Default
     private Integer coinBalance = 0;
+
+    @NotNull
+    @ColumnDefault("0")
+    @Column(name = "is_author", nullable = false)
+    @Builder.Default
+    private Boolean isAuthor = false;
 }
